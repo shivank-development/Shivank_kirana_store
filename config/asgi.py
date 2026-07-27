d70d583f5ece63b54cab2b65265cd2a9a29d1176
𝕊ℍ𝕀𝕍𝔸ℕ𝕂 <officialshivank01@gmail.com>
2026-07-27 20:58:52 +0530

    Update ASGI settings module to use environment variable
    Change default Django settings module to production.

--- config/asgi.py
+++ config/asgi.py
@@ -8,7 +8,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'config.settings.production'))
 
 django_asgi_app = get_asgi_application()
 
